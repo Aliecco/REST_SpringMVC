5db2c9f3a31b56e5e774d75441b0b8f4664852a7
Alisher Rakhymzhan <79269942+Aliecco@users.noreply.github.com>
2022-04-25 22:53:41 +0300

    Add files via upload

---
+++ src/main/java/web/security/spring_security/controller/AdminController.java
@@ -0,0 +1,71 @@
+package web.security.spring_security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.security.spring_security.model.User;
+import web.security.spring_security.service.RoleService;
+import web.security.spring_security.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService service;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService service, RoleService roleService) {
+        this.service = service;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String index(Principal principal,
+                        Model model) {
+        User user = service.findByEmail(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("users", service.findAll());
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "users/admin";
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute("user") User user,
+                         @RequestParam(value = "roles") String[] roles) {
+        System.out.println(user.getFirstName()+user.getLastName()+user.getPassword());
+        user.setRoles(roleService.getSetOfRoles(roles));
+        System.out.println(user.getRoles());
+        service.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@ModelAttribute("user") User user,
+                       Model model,
+                       @PathVariable("id") Long id,
+                       @RequestParam(value = "editRoles") String[] roles) {
+        user.setRoles(roleService.getSetOfRoles(roles));
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("user", service.findById(id));
+        return "users/admin";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String update(@ModelAttribute("user") User user,
+                         @RequestParam(value = "roles", required = false) String[] roles) {
+
+        user.setRoles(roleService.getSetOfRoles(roles));
+        service.updateUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/web/security/spring_security/controller/UserController.java
@@ -0,0 +1,19 @@
+package web.security.spring_security.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.security.spring_security.model.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String show(@AuthenticationPrincipal User user, Model model){
+        model.addAttribute("role", user.getRoles());
+        model.addAttribute("user", user);
+        return "users/admin";
+    }
+}
