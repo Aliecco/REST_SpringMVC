739a0356e9624998744eb2c083d46829e503f508
Aliecco <alisher_r.z@mail.ru>
2022-10-02 21:57:36 +0300

    commented Main

--- src/main/java/web/security/spring_security/SpringSecurityApplication.java
+++ src/main/java/web/security/spring_security/SpringSecurityApplication.java
@@ -15,30 +15,30 @@ import java.util.Set;
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context =
-        SpringApplication.run(SpringSecurityApplication.class, args);
+//        ApplicationContext context =
+                SpringApplication.run(SpringSecurityApplication.class, args);
 
 
-        UserService service = context.getBean(UserService.class);
-        RoleService roleService = context.getBean(RoleService.class);
-
-        Role role1 = new Role("USER");
-        Role role2 = new Role("ADMIN");
-        roleService.addRole(role1);
-        roleService.addRole(role2);
-
-
-        Set<Role> roles1 = new HashSet<>();
-
-        roles1.add(role1);
-        User user1 = new User("user", "user", "User", "Userov", 21);
-        user1.setRoles(roles1);
-        service.saveUser(user1);
-
-        Set<Role> roles2 = new HashSet<>();
-        roles2.add(role2);
-        User user2 = new User("admin", "admin", "Admin", "Adminov",23);
-        user2.setRoles(roles2);
-        service.saveUser(user2);
+//        UserService service = context.getBean(UserService.class);
+//        RoleService roleService = context.getBean(RoleService.class);
+//
+//        Role role1 = new Role("USER");
+//        Role role2 = new Role("ADMIN");
+//        roleService.addRole(role1);
+//        roleService.addRole(role2);
+//
+//
+//        Set<Role> roles1 = new HashSet<>();
+//
+//        roles1.add(role1);
+//        User user1 = new User("user", "user", "User", "Userov", 21);
+//        user1.setRoles(roles1);
+//        service.saveUser(user1);
+//
+//        Set<Role> roles2 = new HashSet<>();
+//        roles2.add(role2);
+//        User user2 = new User("admin", "admin", "Admin", "Adminov",23);
+//        user2.setRoles(roles2);
+//        service.saveUser(user2);
     }
 }
